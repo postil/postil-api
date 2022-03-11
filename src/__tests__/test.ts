74a7465787491c739e9e47bb9f8430a1f0407e16
chakaz <chakaz>
2022-03-11 23:55:13 +0200

    Full API and ok tests

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -2,5 +2,61 @@ import PostilAPI from '../index';
 
 test('listCities', async () => {
   let cities = await PostilAPI.listCities();
-  expect(cities).toStrictEqual(["חיפה", "תל אביב"]);
+  expect(cities).toContainEqual({
+    name: 'עין הוד',
+    symbol: 74,
+    zipcode: '3089000',
+  });
+  expect(cities).toContainEqual({
+    name: 'חיפה',
+    symbol: 4000,
+    syns: ['קריית חיים'],
+  });
+});
+
+test('listStreets', async () => {
+  let streets = await PostilAPI.listStreets('חיפה');
+  expect(streets).toContainEqual({
+    name: 'שד אבא חושי',
+    symbol: 602,
+    houses: 138,
+  });
+  expect(streets).toContainEqual({
+    name: 'שד מוריה',
+    symbol: 625,
+    houses: 130,
+  });
+});
+
+test('listStreetsNonExistingCity', async () => {
+  await expect(PostilAPI.listStreets('עיר לא קיימת')).rejects.toThrow();
+});
+
+test('listHouses', async () => {
+  let houses = await PostilAPI.listHouses('חיפה', 'שד מוריה');
+  expect(houses).toContainEqual({
+    zip: 3457101,
+    pos: '32.802721820678,34.9852665634473',
+    entries: [
+      {
+        entry: 'א',
+        zip: 3457102,
+        pos: '32.802721820678,34.9852665634473',
+      },
+      {
+        entry: 'ב',
+        zip: 3457103,
+        pos: '32.802721820678,34.9852665634473',
+      },
+    ],
+    n: 1,
+  });
+});
+
+test('listHousesNonExistingStreet', async () => {
+  await expect(PostilAPI.listHouses('חיפה', 'רחוב לא קיים')).rejects.toThrow();
+});
+
+test('listHousesNonExistingCity', async () => {
+  await expect(PostilAPI.listHouses('עיר לא קיימת', 'שד מוריה')).rejects.toThrow();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,71 @@
+import axios from 'axios';
+
+export interface City {
+  name: string;
+  zipcode: string;
+  symbol: number;
+  syns: string[];
+}
+
+export interface Street {
+  name: string;
+  symbol: number;
+  houses: number;
+}
+
+const baseUrl = `https://raw.githubusercontent.com/postil/data/main`;
+
 export class PostilAPI {
   listCities() {
-    const promise = new Promise<string[]>((resolve, reject) => {
-      resolve(['חיפה', 'תל אביב']);
+    const url = `${baseUrl}/cities.json`;
+
+    const promise = new Promise<City[]>((resolve, reject) => {
+      axios
+        .get(url)
+        .then((resp) => {
+          resolve(resp.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
     });
+
     return promise;
-  };
+  }
+
+  listStreets(city: string) {
+    const url = `${baseUrl}/${encodeURIComponent(city)}/streets.json`;
+
+    const promise = new Promise<Street[]>((resolve, reject) => {
+      axios
+        .get(url)
+        .then((resp) => {
+          resolve(resp.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+
+    return promise;
+  }
+
+  listHouses(city: string, street: string) {
+    const url = `${baseUrl}/${encodeURIComponent(city)}/${encodeURIComponent(street)}.json`;
+
+    const promise = new Promise<Street[]>((resolve, reject) => {
+      axios
+        .get(url)
+        .then((resp) => {
+          resolve(resp.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+
+    return promise;
+  }
 }
 
 export default new PostilAPI();
